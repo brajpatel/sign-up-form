ce39713552e58a61b6f9542a469f00592dc9e7c6
brajpatel <brajeshpatel0002@gmail.com>
2022-07-08 16:47:22 +0100

    Complete password confirm match function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const button = document.getElementById('create-account')
 const password = document.getElementById('password')
+const passwordLabel = document.getElementById('password-label')
 const confirmPassword = document.getElementById('confirm_password')
-
+const confirmPasswordLabel = document.getElementById('confirm-password-label')
+const checkMark = '&#10004;'
 
 password.addEventListener('keyup', passwordConfirmation)
 confirmPassword.addEventListener('keyup', passwordConfirmation)
@@ -10,12 +12,20 @@ function passwordConfirmation() {
     let passwordValue = document.getElementById('password').value
     let confirmPasswordValue = document.getElementById('confirm_password').value
 
-    if(confirmPasswordValue === passwordValue) {
+    if(confirmPasswordValue === passwordValue && passwordValue !== '' && confirmPasswordValue !== '') {
         password.style.border = "3px solid green"
+        passwordLabel.innerHTML = "Password " + checkMark
         confirmPassword.style.border = "3px solid green"
+        confirmPasswordLabel.innerHTML = "Confirm Password " + checkMark
     }
-    else if (passwordValue !== confirmPasswordValue) {
+    else if (passwordValue !== confirmPasswordValue && passwordValue !== '' && confirmPasswordValue !== '') {
         password.style.border = "3px solid red"
+        passwordLabel.innerHTML = "Password"
         confirmPassword.style.border = "3px solid red"
+        confirmPasswordLabel.innerHTML = "Confirm Password"
+    }
+    else {
+        password.style.border = "2px solid rgb(107, 107, 107)"
+        confirmPassword.style.border = "2px solid rgb(107, 107, 107)"
     }
 }
