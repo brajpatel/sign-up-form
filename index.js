94cb703397d52410e369adecff5ceb1f59981855
brajpatel <brajeshpatel0002@gmail.com>
2022-07-09 13:59:02 +0100

    Add cross mak to non-matching passwords

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const passwordLabel = document.getElementById('password-label')
 const confirmPassword = document.getElementById('confirm_password')
 const confirmPasswordLabel = document.getElementById('confirm-password-label')
 const checkMark = '&#10004;'
+const crossMark = '&#10060;'
 
 password.addEventListener('keyup', passwordConfirmation)
 confirmPassword.addEventListener('keyup', passwordConfirmation)
@@ -20,9 +21,9 @@ function passwordConfirmation() {
     }
     else if (passwordValue !== confirmPasswordValue && passwordValue !== '' && confirmPasswordValue !== '') {
         password.style.border = "3px solid red"
-        passwordLabel.innerHTML = "Password"
+        passwordLabel.innerHTML = "Password " + crossMark
         confirmPassword.style.border = "3px solid red"
-        confirmPasswordLabel.innerHTML = "Confirm Password"
+        confirmPasswordLabel.innerHTML = "Confirm Password " + crossMark
     }
     else {
         password.style.border = "2px solid rgb(107, 107, 107)"
